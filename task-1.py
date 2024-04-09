7123e819e338274164585db8273beedd741f1b86
Nurislamics <166114766+Nurislamics@users.noreply.github.com>
2024-04-09 14:23:50 +0500

    Add files via upload

---
+++ task-1.py
@@ -0,0 +1,12 @@
+def Pifagor(a, b): 
+    import math
+    a = int(a)
+    b = int(b)
+    c = math.sqrt(a**2 + b**2)
+    print("Гипотенуза треугольника: ", c) 
+print("\tВычисление длины гипотенузы") 
+a = input("Введите первую значение катета: ") 
+b = input("Введите вторую значение катета: ") 
+Pifagor(a, b)
+    
+    
